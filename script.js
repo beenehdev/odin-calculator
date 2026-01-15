8352cc8b8f16b1ea859f512a7939a745f23f0230
beenehdev <beenehdev@gmail.com>
2026-01-14 18:32:27 -0800

    Add baseline calculator logic

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 "use strict";
+
+let a = null;
+let b = null;
+let operatorSymbol = 0;
+
+function sum(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
+function operate(a, b, operatorSymbol) {
+    switch (operatorSymbol) {
+        case 0:
+            break;
+        case 1:
+            sum(a, b);
+            break;
+        case 2:
+            subtract(a, b);
+            break;
+        case 3:
+            multiply(a, b);
+            break;
+        case 4:
+            divide(a, b);
+            break;
+    };
+};
